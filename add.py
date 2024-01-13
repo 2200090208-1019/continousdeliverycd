924057f2f058a921c5eb5d5faa36be8ed828df08
indusri <you@indu.com>
2024-01-13 12:07:34 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,8 @@
+# This program add two numbers
+num1 = 1.5
+nun2 = 6.3
+# Add tow numbers
+sum = sum1+sum2um = num1 + num2
+
+#Display the sum
+print()[D'The sum of {0} and {1} is {2}'.format()[Dnum1, num2, sum)
